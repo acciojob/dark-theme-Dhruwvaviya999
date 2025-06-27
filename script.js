429c250cf9b4c7437584f57b3b2bb47fc3d84060
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-06-28 01:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
 
+let isDark = false;
+
+const swapBtn = document.getElementById("swap");
+
+function swapTheme(){
+	isDark = !isDark;
+	const main = document.getElementById("app");
+	if(isDark){
+		main.classList.value = "night";
+		swapBtn.classList.value = "button_night";
+	} else {
+		main.classList.value = "day";
+		swapBtn.classList.value = "button_day";
+	}
+}
+
+
+swapBtn.onclick = swapTheme;
+
